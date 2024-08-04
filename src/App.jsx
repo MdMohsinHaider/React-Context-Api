b5c18b910e39050fdb392ebbcdd75ca18d29f98d
Md Mohsin Haider <98874649+MdMohsinHaider@users.noreply.github.com>
2024-08-04 05:39:36 +0000

    feature/one is compl..

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
 
   return (
-    <>
+    <UserContextProvider>
       <h1>React With Md Mohsin haider</h1>
-    </>
+    </UserContextProvider>
   )
 }
 
